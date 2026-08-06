be65ef1ef6ce6c778edd1cfb4760a0c7fee271aa
eXtreme4 <extreme4.music@gmail.com>
2026-08-06 03:48:08 +0200

    Add new DSP components and update exports

--- neurafs/core/__init__.py
+++ neurafs/core/__init__.py
@@ -1,16 +1,26 @@
 """NeuraFS Core Engine Components."""
 
-from neurafs.core.config import config, PrecisionMode
+from neurafs.core.config import config, PrecisionMode, DecodeMode, TargetQualityTier
 from neurafs.core.container import HCSContainer
-from neurafs.core.dsp import DSPProcessor
+from neurafs.core.dsp import (
+    AudioLoader,
+    SpectralComplexityAnalyzer,
+    SubbandFilterBank,
+    TemporalChunker,
+)
 from neurafs.core.engine import NeuraFSEngine
 from neurafs.core.exceptions import NeuraFSError
 
 __all__ = [
     "config",
     "PrecisionMode",
+    "DecodeMode",
+    "TargetQualityTier",
     "HCSContainer",
-    "DSPProcessor",
+    "AudioLoader",
+    "SpectralComplexityAnalyzer",
+    "SubbandFilterBank",
+    "TemporalChunker",
     "NeuraFSEngine",
     "NeuraFSError",
 ]
